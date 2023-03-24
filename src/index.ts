b93ffc01c3414ff66a17830cf0c5738dc81d86b0
HiromiShikata <shikata.hiromi@gmail.com>
2023-03-24 19:40:52 +0000

    chore: publish v1.0.0 / close #5

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-export const hello = (name: string) => `hello ${name}`;
+import { Command } from 'commander';
+import { GetDefinitionByPathUseCase } from './domain/usecases/GetDefinitionByPathUseCase';
+import { TsMorphEntityDefinitionRepository } from './adapter/repositories/TsMorphEntityDefinitionRepository';
+
+const program = new Command();
+program
+  .command('get definition <path>')
+  .description('Get entity definitions and relation definitions')
+  .action(async (path: string) => {
+    const useCase = new GetDefinitionByPathUseCase(
+      new TsMorphEntityDefinitionRepository(),
+    );
+    const res = await useCase.run(path);
+    console.log(JSON.stringify(res));
+  });
+program.parse(process.argv);
