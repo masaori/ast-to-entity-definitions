a90b3c69e8228fb162cbc3325917023dbce960ea
HiromiShikata <shikata.hiromi@gmail.com>
2023-03-26 20:55:55 +0000

    fix: add shebang to entrypoint for cli

--- src/adapter/entry-points/cli/index.ts
+++ src/adapter/entry-points/cli/index.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import { Command } from 'commander';
 import { GetDefinitionByPathUseCase } from '../../../domain/usecases/GetDefinitionByPathUseCase';
 import { TsMorphEntityDefinitionRepository } from '../../repositories/TsMorphEntityDefinitionRepository';
