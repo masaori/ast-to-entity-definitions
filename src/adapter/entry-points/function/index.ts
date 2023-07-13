903a87f03a9ae0e2569cceb82b00425e1d88f69b
Masaori Hirono <masaori.hirono@gmail.com>
2023-07-13 17:06:17 -0500

    Fixed export method so user no need to refer the path directry

--- src/adapter/entry-points/function/index.ts
+++ src/adapter/entry-points/function/index.ts
@@ -1,1 +1,7 @@
+// Entities
+export * from '../../../domain/entities/EntityDefinition';
+export * from '../../../domain/entities/EntityPropertyDefinition';
+export * from '../../../domain/entities/Unique';
+
+// Use Cases
 export * from './getEntityDefinitions';
